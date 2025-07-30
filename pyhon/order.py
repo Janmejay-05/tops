6eb8976875a52976d9d83065ee6051eb355d882d
Janmejaysinh Zala <janmejayazala@gmail.com>
2025-07-30 17:51:29 +0530

    new

--- pyhon/order.py
+++ pyhon/order.py
@@ -1,14 +1,16 @@
 #Order code
 
 total = int(input("kitne ka liyaaaaa?"))
-is_first_order = input("is this your first order")
+is_first_order = input("enter Y if yes , enter N is no")
 
 print(is_first_order,type(is_first_order))
 
 if total>=1000:
-    if is_first_order==True:
+    if is_first_order=="Y":
         print("you got 300 rupeess, aishh kar ja")
-    elif is_first_order==False:
+    elif is_first_order=="N":
         print("dusri bar order kiya koy nay 300 nay 100 ka de dunga, kyo ki 1000 se jyada hain naa")
+    else:
+        print("wrong input")
 else:
     print("1000 se jyada kar na kanjoosssss")
