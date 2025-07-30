310433fd50d9a2628073d0231d587442a2285ba2
Janmejaysinh Zala <janmejayazala@gmail.com>
2025-07-30 17:57:28 +0530

    assesment

---
+++ pyhon/assesment/start.py
@@ -0,0 +1,27 @@
+name=input("enter your name")
+print("Welcome ",name)
+
+# addition of two numbers
+
+a=int(input("enter number one"))
+b=int(input("enter number two"))
+
+
+# printing all arithmetic operators
+print("the addition of ",a," and ",b," is ",a+b)
+print("the subtraction of ",a," and ",b," is ",a-b)
+print("the multiplication   of ",a," and ",b," is ",a*b)
+print("the division of ",a," and ",b," is ",a/b)
+
+# printing all comparision  operators
+
+print("if a is equal to b ",a==b)
+print("if a is greater than b ",a>b)
+print("if b is greater than a",a<b)
+print("or operator ",a==b or a<b)
+print("and operator ",a>b and a>5)
+
+#checking the type of a and b
+
+print("the type of a is ",type(a))
+print("the type of b is ",type(b))
